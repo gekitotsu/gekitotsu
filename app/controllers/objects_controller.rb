54cb79e216c41a44c6ed5afa92f7e51d0da1212f
T.K <>
2018-03-29 21:24:51 +0900

    add show-route, controller, view

--- app/controllers/objects_controller.rb
+++ app/controllers/objects_controller.rb
@@ -1,4 +1,7 @@
 class ObjectsController < ApplicationController
   def index
   end
+
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
 
   root 'objects#index'
 
+  get 'object/show', to: 'objects#show'
+  # resources :objects
 end
